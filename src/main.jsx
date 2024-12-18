07da18aaed72b996520768bb6751e27c33417a48
Abhijai Srivastava <abhijai.219301464@muj.manipal.edu>
2024-12-18 16:52:53 +0530

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app'
+import App from './App';
 import {Provider } from 'react-redux'
 import {store} from './services/store'
 ReactDOM.createRoot(document.getElementById('root')).render(
